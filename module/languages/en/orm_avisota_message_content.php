f359cd44de7690eac87850bc4430d3fb228b5b10
Tristan Lins <tristan.lins@bit3.de>
2014-02-21 02:14:08 +0100

    More refactoring and splitting.

---
+++ module/languages/en/orm_avisota_message_content.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Avisota newsletter and mailing system
+ * Copyright (C) 2013 Tristan Lins
+ *
+ * PHP version 5
+ *
+ * @copyright  bit3 UG 2013
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @package    avisota/contao-core
+ * @license    LGPL-3.0+
+ * @filesource
+ */
+
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['orm_avisota_message_content']['tableItems']   = array(
+	'Table Entries ',
+	'If JavaScript is disabled, you must save your changes until you change the order.'
+);
+$GLOBALS['TL_LANG']['orm_avisota_message_content']['tableSummary'] = array(
+	'Summary ',
+	' Please enter a brief summary of the content and structure of the table.'
+);
+$GLOBALS['TL_LANG']['orm_avisota_message_content']['tableHeader']  = array(
+	'Add header',
+	'The first row of the table to use as the header.'
+);
+$GLOBALS['TL_LANG']['orm_avisota_message_content']['tableFooter']  = array(
+	'Add footer',
+	'The last row of the table to use as a footer.'
+);
+
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['orm_avisota_message_content']['table_legend']   = 'Table Entries';
+$GLOBALS['TL_LANG']['orm_avisota_message_content']['tconfig_legend'] = 'Table Settings';
