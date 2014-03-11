8a3b892367d7ab1e8cf56128d3210f36533774f9
Tristan Lins <tristan.lins@bit3.de>
2014-03-11 15:57:40 +0100

    Add module description.

---
+++ module/languages/en/modules.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Avisota newsletter and mailing system
+ * Copyright (C) 2013 Tristan Lins
+ *
+ * PHP version 5
+ *
+ * @copyright  bit3 UG 2013
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @package    avisota/contao-message-element-table
+ * @license    LGPL-3.0+
+ * @filesource
+ */
+
+
+/**
+ * Module
+ */
+$GLOBALS['TL_LANG']['MOD']['avisota-message-element-table'] = array(
+	'Avisota - Message element "Table"',
+	'"Table" message element for Avisota messages.'
+);
