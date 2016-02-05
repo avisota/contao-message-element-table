f71edeeb568bbaa6cf11850b9eb27d10497ddab1
Sven Baumann <baumann.sv@gmail.com>
2016-02-05 13:07:42 +0100

    reform code

--- contao/dca/orm_avisota_message_content.php
+++ contao/dca/orm_avisota_message_content.php
@@ -17,8 +17,7 @@
  * Table orm_avisota_message_content
  * Entity Avisota\Contao:MessageContent
  */
-$GLOBALS['TL_DCA']['orm_avisota_message_content']['metapalettes']['table'] = array
-(
+$GLOBALS['TL_DCA']['orm_avisota_message_content']['metapalettes']['table'] = array(
     'type'      => array('cell', 'type', 'headline'),
     'table'     => array('tableItems'),
     'tconfig'   => array('tableSummary', 'tableHeader', 'tableFooter'),
@@ -26,30 +25,29 @@ $GLOBALS['TL_DCA']['orm_avisota_message_content']['metapalettes']['table'] = arr
     'published' => array('invisible'),
 );
 
-$GLOBALS['TL_DCA']['orm_avisota_message_content']['fields']['tableItems']   = array
-(
+$GLOBALS['TL_DCA']['orm_avisota_message_content']['fields']['tableItems'] = array(
     'label'     => &$GLOBALS['TL_LANG']['orm_avisota_message_content']['tableItems'],
     'exclude'   => true,
     'inputType' => 'tableWizard',
     'eval'      => array('allowHtml' => true, 'doNotSaveEmpty' => true, 'style' => 'width:142px; height:66px;')
 );
-$GLOBALS['TL_DCA']['orm_avisota_message_content']['fields']['tableSummary'] = array
-(
+
+$GLOBALS['TL_DCA']['orm_avisota_message_content']['fields']['tableSummary'] = array(
     'label'     => &$GLOBALS['TL_LANG']['orm_avisota_message_content']['tableSummary'],
     'exclude'   => true,
     'search'    => true,
     'inputType' => 'text',
     'eval'      => array('mandatory' => true, 'maxlength' => 255)
 );
-$GLOBALS['TL_DCA']['orm_avisota_message_content']['fields']['tableHeader']  = array
-(
+
+$GLOBALS['TL_DCA']['orm_avisota_message_content']['fields']['tableHeader'] = array(
     'label'     => &$GLOBALS['TL_LANG']['orm_avisota_message_content']['tableHeader'],
     'exclude'   => true,
     'inputType' => 'checkbox',
     'eval'      => array('tl_class' => 'w50')
 );
-$GLOBALS['TL_DCA']['orm_avisota_message_content']['fields']['tableFooter']  = array
-(
+
+$GLOBALS['TL_DCA']['orm_avisota_message_content']['fields']['tableFooter'] = array(
     'label'     => &$GLOBALS['TL_LANG']['orm_avisota_message_content']['tableFooter'],
     'exclude'   => true,
     'inputType' => 'checkbox',
