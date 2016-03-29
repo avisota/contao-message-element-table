b8ae835206f95908ab88ff12ecd79cf9c1ef13c2
Sven Baumann <baumann.sv@gmail.com>
2016-03-29 14:29:03 +0200

    composer requirement for avisota/contao-core, and fix the contao/core isn`t installed

--- tests/composer_update.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-composer remove avisota/contao-message --no-update
-composer remove avisota/contao-message-element-headline --no-update
-composer update --prefer-dist --no-interaction
-composer require avisota/contao-message 3.2.x-dev --no-update
-composer require avisota/contao-message-element-headline 3.1.x-dev
