ccc046be38cce2972d41d7cc3077fbdcfa1522f8
Sven Baumann <baumann.sv@gmail.com>
2016-03-29 13:38:20 +0200

    fixed requirements in composer update script

--- tests/composer_update.sh
+++ tests/composer_update.sh
@@ -3,5 +3,5 @@
 composer remove avisota/contao-message --no-update
 composer remove avisota/contao-message-element-headline --no-update
 composer update --prefer-dist --no-interaction
-composer require avisota/contao-message-element 3.2.x-dev --no-update
+composer require avisota/contao-message 3.2.x-dev --no-update
 composer require avisota/contao-message-element-headline 3.1.x-dev
