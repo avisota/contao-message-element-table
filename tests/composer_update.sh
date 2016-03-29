5889e97b304bf8b7cafeb4adba7bb46e2292e051
Sven Baumann <baumann.sv@gmail.com>
2016-03-29 08:09:41 +0200

    update requirements for dev-release

--- tests/composer_update.sh
+++ tests/composer_update.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+composer remove avisota/contao-message --no-update
 composer remove avisota/contao-message-element-headline --no-update
 composer update --prefer-dist --no-interaction
-composer require avisota/contao-message-element-headline ~3.0
+composer require avisota/contao-message-element dev-release/3.2.0  --no-update
+composer require avisota/contao-message-element-headline dev-release/3.1.0
